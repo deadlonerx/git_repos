8ab456a12e71020d62e66eb0f79d3d4eedb115b9
deadloner <deadlonerx@mail.ru>
2025-06-01 20:24:11 +0300

    Script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - синтаксис 
 - qweqwe
 - ghjkj
-
+-commit dlya skripta
 
 
 - 6123 
---
+++ script.ps1
@@ -0,0 +1,21 @@
+$url = "git@github.com:deadlonerx/git_repos.git"
+$path = "D:\git_repos\1"
+git clone $url $path
+$desc = git describe --tags
+Write-Output $desc
+if ($desc.Contains("-")){
+	$data = $desc -split '\-'
+	$ver = $data[0] -split '\.'
+	$major = $ver[0]
+	$minor = $ver[1]
+	$patch = [int]$ver[2] + 1
+	$newTag = "$major.$minor.$patch"
+	Write-Output $newTag
+	Set-Location $path
+	git tag -a $newTag -m "Version $newTag"
+	git push origin $newTag
+	Start-Sleep -Seconds 5
+} else {
+	Write-Output "No changes"
+}
+Remove-Item -Path $path -Recurse -Force
